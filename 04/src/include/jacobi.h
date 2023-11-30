ac1f5060c710ed14ffbf46ee35f475e7aa6f37af
Turnyur <turnyur@lambda.fritz.box>
2023-11-30 10:59:30 +0100

    add gprof and likwid profiling impl

--- 04/src/include/jacobi.h
+++ 04/src/include/jacobi.h
@@ -7,5 +7,6 @@
 /* This function iterates over a grid to perform a jacobi time step.
  */
 void jacobi(double* grid_source, double* grid_target, uint32_t x, uint32_t y);
+void jacobi_column_wise(double* grid_source, double* grid_target, uint32_t x, uint32_t y);
 
 #endif
