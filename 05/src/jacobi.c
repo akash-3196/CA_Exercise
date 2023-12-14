c9f95915c5ef67e4c730b3d8aa764bd0fcddf44f
Turnyur <turnyur@lambda.fritz.box>
2023-12-14 18:28:01 +0100

    fix: minor bug in jacobi_spatial_block

--- 05/src/jacobi.c
+++ 05/src/jacobi.c
@@ -7,7 +7,7 @@
 
 // Uncomment the following line to enable twofold unrolling
 #define JACOBI_UNROLLING 4
-#define BLOCKING_FACTOR 512
+#define BLOCKING_FACTOR 32
 
 
 void jacobi(double* grid_source, double* grid_target, uint32_t x, uint32_t y) {
@@ -215,36 +215,30 @@ void jacobi_column_wise(double* grid_source, double* grid_target, uint32_t x, ui
 
 void jacobi_spatial_blocking(double* grid_source, double* grid_target, uint32_t x, uint32_t y) {
 
+   
     for (uint32_t i = 1; i < y - 1; ++i) {
-
-       
-        for (uint32_t j = 1; j < x - 1; j += BLOCKING_FACTOR) {
-
+    for (uint32_t j = 1; j < x - 1; j += BLOCKING_FACTOR) {
         #pragma novector
         #pragma nounroll
+        for (uint32_t k = 0; k < BLOCKING_FACTOR; k += JACOBI_UNROLLING) {
+            uint32_t offset = j + k;
+            
+            // Load data using AVX
+            __m256d top_point = _mm256_loadu_pd(&grid_source[x * (i - 1) + offset]);
+            __m256d bottom_point = _mm256_loadu_pd(&grid_source[x * (i + 1) + offset]);
 
-            for (uint32_t k = 0; k < BLOCKING_FACTOR; k += JACOBI_UNROLLING) {
-              
-              uint32_t offset = j + k;
-                    __m256d top_point = _mm256_loadu_pd(&grid_source[x * (i - 1) + offset]);
-                    __m256d bottom_point = _mm256_loadu_pd(&grid_source[x * (i + 1) + offset]);
+            __m256d left_point = _mm256_loadu_pd(&grid_source[x * i + (offset - 1)]);
+            __m256d right_point = _mm256_loadu_pd(&grid_source[x * i + (offset + 1)]);
 
-                    __m256d left_point = _mm256_loadu_pd(&grid_source[x * i + (offset - 1)]);
-                    __m256d right_point = _mm256_loadu_pd(&grid_source[x * i + (offset + 1)]);
-
-                    __m256d result1 = _mm256_add_pd(top_point, bottom_point);
-                    __m256d result2 = _mm256_add_pd(left_point, right_point);
-
-                    __m256d total_points = _mm256_add_pd(result1, result2);
-
-                    total_points = _mm256_mul_pd(total_points, _mm256_set1_pd(0.25));
-
-                    _mm256_storeu_pd(&grid_target[x * i + j], total_points);
-
-
-            }
+            // Perform the Jacobi computation and store the results at different offsets
+            _mm256_storeu_pd(&grid_target[x * i + offset], _mm256_mul_pd(
+                _mm256_add_pd(top_point, bottom_point) + _mm256_add_pd(left_point, right_point),
+                _mm256_set1_pd(0.25)
+            ));
         }
     }
+}
+   
 }
 
 
